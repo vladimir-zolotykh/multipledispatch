0fd46437889a3b4a9c497727c0c057dd67ddb31b
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-04-13 19:18:43 +0300

    Add first MultiMethod descriptor: addxs function

--- multimethod.py
+++ multimethod.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+import types
 
 
 class MultiMethod:
-    pass
+    _functions = {}
+
+    def __init__(self, name=None):
+        self._name = name
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def add_function(self, func):
+        self._functions[func.__name__] = func
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        return types.MethodType(self._functions["addxs"], instance)
 
 
-class MultiDict:
-    pass
+def addxs(self, x: int, s: str):
+    return str(x) + s
 
 
-class MultiMeta(type):
-    pass
+class Spam:
+    sum = MultiMethod()
+    sum.add_function(addxs)
+
+    def __call__(self, x: int, s: str):
+        print(self.sum(x, s))
+
+
+if __name__ == "__main__":
+    spam = Spam()
+    spam(11, " hello")
